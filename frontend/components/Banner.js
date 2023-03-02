cc2b847816b5a24836074e74eed0164e935c38f0
Arthur Vera <arthur@mcube.fr>
2023-03-02 11:02:59 +0100

    Fix rendered

--- frontend/components/Banner.js
+++ frontend/components/Banner.js
@@ -2,13 +2,19 @@ import styles from '../styles/Banner.module.scss';
 import bg from '../public/images/bg.jpg';
 import vera from '../public/vera.svg';
 import Image from "next/image";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import gsap from "gsap/dist/gsap";
 import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
 import {SplitText} from "gsap/dist/SplitText";
 
 
 const Banner = () => {
+
+	const [hasRendered, setHasRendered] = useState(false);
+
+	useEffect(() => {
+		setHasRendered(true);
+	}, []);
 
     if (typeof window !== 'undefined') {
         gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -19,24 +25,26 @@ const Banner = () => {
     const banner = useRef(null);
 
     useEffect(() => {
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: banner.current,
-                start: 'bottom bottom',
-                end: 'bottom top',
-                scrub: true,
-                markers: true,
-                pin: true,
-            },
-        });
+		if (hasRendered) {
+			const tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: banner.current,
+					start: 'bottom bottom',
+					end: 'bottom top',
+					scrub: true,
+					markers: true,
+					pin: true,
+				},
+			});
 
-        tl.set(introRef.current, {autoAlpha: 1})
+			tl.set(introRef.current, {autoAlpha: 1})
 
-        tl.to("#navbar", {y: -100, duration: .3, ease: "power2.out"})
-            .to(introRef.current, {y: 100, duration: .3, autoAlpha: 0, ease: "power2.out"})
-            .to(containerImgRef.current, {scale: .7, duration: .3, ease: "power2.out"})
+			tl.to("#navbar", {y: -100, duration: .3, ease: "power2.out"})
+				.to(introRef.current, {y: 100, duration: .3, autoAlpha: 0, ease: "power2.out"})
+				.to(containerImgRef.current, {scale: .7, duration: .3, ease: "power2.out"})
+		}
 
-    }, []);
+    }, [hasRendered]);
 
 
 
