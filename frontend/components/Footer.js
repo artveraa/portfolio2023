8038a707be344a0e7f8476aae74f4f06062a8652
artvera <arthurvera@protonmail.com>
2023-05-03 16:34:30 +0200

    Footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,23 +1,75 @@
 import styles from '@/styles/Footer.module.scss';
 import Link from "next/link";
+import {useEffect, useRef, useState} from "react";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
+
 
 const Footer = () => {
+
+    const [hasRendered, setHasRendered] = useState(false);
+
+    useEffect(() => {
+        setHasRendered(true)
+    });
+
+    if (typeof window !== 'undefined') {
+        gsap.registerPlugin(ScrollTrigger);
+
+    }
+
+
+    const footerRef = useRef(null);
+    const surnameRef = useRef(null);
+    const veraRef = useRef(null);
+    const getInTouchRef = useRef(null);
+    // const socialItems = document.querySelectorAll('.social-item');
+
+
+
+    useEffect(() => {
+        if (!hasRendered) return;
+
+        gsap.set(surnameRef.current, {x: '-100%', ease: 'power2.out', autoAlpha: 0, scale: 2});
+        gsap.set(veraRef.current, {x: '100%', ease: 'power2.out', autoAlpha: 0, scale: 2});
+        // gsap.set(getInTouchRef.current, {ease: 'power2.out', autoAlpha: 0});
+        // gsap.set(socialItems, {autoAlpha: 0, x: 70});
+
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: footerRef.current,
+                start: 'top 80%',
+                end: 'bottom 30%',
+                scrub: true,
+                // markers: true,
+            }
+        });
+
+        tl.to(surnameRef.current, {x: '0%', duration: 1, ease: 'power2.out', autoAlpha: 1, scale: 1})
+            .to(veraRef.current, {x: '0%', duration: 1, ease: 'power2.out', autoAlpha: 1, scale: 1}, '-=1')
+            .to(getInTouchRef.current, {autoAlpha: 1, duration: 1, ease: 'power2.out'}, '+=.5')
+            // .to(socialItems, {autoAlpha: 1, x: 0, duration: 1, ease: 'power2.out', stagger: .2})
+
+
+    }, [hasRendered]);
+
     return (
-        <footer className={styles.footer}>
+        <footer className={styles.footer} ref={footerRef}>
             <div className={styles.footer__container}>
                 <div className={styles.footer__left}>
-                    <Link href={"mailto:arthurvera96@gmail.com"}>Get in touch</Link>
+                    <Link href={"mailto:arthurvera96@gmail.com"} ref={getInTouchRef}>Get in touch <span>👋</span></Link>
                 </div>
 
                 <div className={styles.footer__right}>
-                    <Link href={"https://www.linkedin.com/in/arthurvera/"}>LinkedIn</Link>
-                    <Link href={""}>Github</Link>
-                    <Link href={""}>Twitter</Link>
+                    <Link href={"https://www.linkedin.com/in/arthurvera/"} className={'social-item'}>LinkedIn</Link>
+                    <Link href={""} className={'social-item'}>Github</Link>
+                    <Link href={""} className={'social-item'}>Twitter</Link>
                 </div>
             </div>
             <div className={styles.footer__bottom}>
-                <div className={styles.surname}>Arthur</div>
-                <div className={styles.name}>Vera</div>
+                <div className={styles.surname} ref={surnameRef}>Arthur</div>
+                <div className={styles.name} ref={veraRef}>Vera</div>
             </div>
         </footer>
     );
